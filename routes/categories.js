afdaaca53995f1ff379ea8e9f47bd540d805cf0b
Sandeep Shukla <sandeepini.2012@gmail.com>
2020-08-06 22:50:16 +0530

    5 Commit add post with editor and can add category also

--- routes/categories.js
+++ routes/categories.js
@@ -3,17 +3,6 @@ var router = express.Router();
 var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
-router.get('/show/:category', function(req, res, next) {
-	var posts = db.get('posts');
-
-	posts.find({category: req.params.category},{},function(err, posts){
-		res.render('index',{
-  			'title': req.params.category,
-  			'posts': posts
-  		});
-	});
-});
-
 router.get('/add', function(req, res, next) {
 	res.render('addcategory',{
   		'title': 'Add Category'	
